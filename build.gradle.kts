bc13832a44fb282f63d583357f8a3dcfcdf90368
Aidan Heaslip <sientyhd89@outlook.com.au>
2025-03-12 21:18:28 +1000

    added listener

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,16 +2,22 @@ plugins {
     id("java")
 }
 
-group = "net.veroud"
+group = "com.veroud"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
 }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    compileOnly("com.velocitypowered:velocity-api:3.4.0-SNAPSHOT")
+    annotationProcessor("com.velocitypowered:velocity-api:3.4.0-SNAPSHOT")
 }
 
 tasks.test {
--- src/main/java/com/veroud/ChoEazyTab/ChoEazyTab.java
+++ src/main/java/com/veroud/ChoEazyTab/ChoEazyTab.java
@@ -1,23 +1,46 @@
-package net.veroud.ChoEazyTab;
+package com.veroud.ChoEazyTab;
 
 import com.google.inject.Inject;
+import com.velocitypowered.api.event.PostOrder;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.player.PlayerChatEvent;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.ProxyServer;
 import org.slf4j.Logger;
 
-@Plugin(id = "myfirstplugin", name = "My First Plugin", version = "0.1.0-SNAPSHOT",
-        url = "https://example.org", description = "I did it!", authors = {"Me"})
+import java.nio.file.Path;
+
+@Plugin(id = "choeazytab", name = "ChoEazyTab", version = "0.1.0-SNAPSHOT",
+        url = "https://veroud.com", description = "I did it!", authors = {"Me"})
 public class ChoEazyTab {
 
     private final ProxyServer server;
     private final Logger logger;
+    private final Path dataDirectory;
 
     @Inject
-    public ChoEazyTab(ProxyServer server, Logger logger) {
+    public ChoEazyTab(ProxyServer server, Logger logger, @DataDirectory Path dataDirectory) {
         this.server = server;
         this.logger = logger;
+        this.dataDirectory = dataDirectory;
 
         logger.info("MrAteyo was here!");
         logger.info("Loading ChoEazyTab.");
+    }
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+        // Do some operation demanding access to the Velocity API here.
+        // For instance, we could register an event:
+        server.getEventManager().register(this, new ChoEazyTabListener());
+    }
+    public class ChoEazyTabListener {
+
+        @Subscribe(order = PostOrder.EARLY)
+        public void onPlayerChat(PlayerChatEvent event) {
+            // do something here
+        }
+
     }
 }
